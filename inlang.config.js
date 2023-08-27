27152a16769b5304728a687c2004832025b9fcac
mbojanks <bmilosavljevic@gmail.com>
2023-08-27 11:47:47 +0200

    initial commit

---
+++ inlang.config.js
@@ -0,0 +1,37 @@
+/**
+* @type { import("@inlang/core/config").DefineConfig }
+*/
+export async function defineConfig(env) {
+	const { default: jsonPlugin } = await env.$import(
+		"https://cdn.jsdelivr.net/npm/@inlang/plugin-json@3/dist/index.js",
+	)
+
+	return {
+		referenceLanguage: "en",
+		plugins: [
+			jsonPlugin({
+				pathPattern: "./src/{language}.json",
+			}),
+		],
+	}
+}
+export async function defineConfig(env) {
+	const { default: pluginJson } = await env.$import(
+		"https://cdn.jsdelivr.net/npm/@inlang/plugin-json@3/dist/index.js",
+	)
+
+	// recommended to enable linting feature
+	const { default: standardLintRules } = await env.$import(
+		"https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js",
+	)
+
+	return {
+		referenceLanguage: "en",
+		plugins: [
+			pluginJson({
+				pathPattern: "./src/{language}.json",
+			}),
+			standardLintRules(),
+		],
+	}
+}
